2d46be190b9d2a72e899932b25d7770241c13631
Grégoire Deveaux <ggdx@laposte.net>
2023-07-01 19:38:57 +0200

    job

--- fastapi_start/job_inject.py
+++ fastapi_start/job_inject.py
@@ -10,23 +10,36 @@ from fastapi.dependencies.utils import get_typed_signature
 from fastapi.dependencies.utils import is_gen_callable
 
 
-def inject(func):
+def inject(func: Callable[..., Any]) -> Callable[..., Any]:
     @functools.wraps(func)
-    def resolved_func(*args, **kwargs):
+    def resolved_func(*args: Any, **kwargs: dict[str, Any]) -> Any:
         with ExitStack() as exit_stack:
-            return sub_inject(func, exit_stack)(*args, **kwargs)
+            return _call(_inject(func, exit_stack), exit_stack, *args, **kwargs)
 
     return resolved_func
 
 
-def sub_inject(func: Callable[..., Any], exit_stack: ExitStack) -> Callable[..., Any]:
+def _inject(func: Callable[..., Any], exit_stack: ExitStack) -> Callable[..., Any]:
     return functools.partial(
         func,
         **{
-            param.name: _resolve_dependency(dependency, exit_stack)
+            param.name: _call(_inject(dependency, exit_stack), exit_stack)
             for param in get_typed_signature(func).parameters.values()
             if (dependency := _get_param_dependency(param))
         },
+    )
+
+
+def _call(
+    func: Callable[..., Any],
+    exit_stack: ExitStack,
+    *args: Any,
+    **kwargs: dict[str, Any],
+) -> Any:
+    return (
+        exit_stack.enter_context(contextmanager(func)(*args, **kwargs))
+        if is_gen_callable(func)
+        else func(*args, **kwargs)
     )
 
 
@@ -38,11 +51,3 @@ def _get_param_dependency(param: Parameter) -> Callable[..., Any] | None:
         is_path_param=False,
     )
     return depends.dependency if depends else None
-
-
-def _resolve_dependency(dependency: Callable[..., Any], exit_stack: ExitStack) -> Any:
-    if is_gen_callable(dependency):
-        return exit_stack.enter_context(
-            contextmanager(sub_inject(dependency, exit_stack))()
-        )
-    return sub_inject(dependency, exit_stack)()
